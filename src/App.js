9125a7ff5d6a51ebd19e0beeb21d7d615928c72b
ACarrill42 <austincarrill@gmail.com>
2021-02-05 22:47:49 -0600

    working on styling components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import './App.css';
 import axios from 'axios';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  // State
   const [characters,setCharacters] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  // Fetching info from Star Wars API
   const fetchSWA = () => {
     axios.get('https://swapi.dev/api/people/')
     .then(res => {
@@ -26,7 +23,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      { characters.map(info => <Character key ={info.name} props ={info}/>) }
+      { characters.map(info => <Character key = {info.name} props = {info}/>) }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,34 @@
 // Write your Character component here
 import React from 'react';
+import styled from 'styled-components';
 
+// Color is from https://imperialstyleguide.com/#colors
+const StyledDiv = styled.div `
+    h3{
+        background-color: #FFFFFF;
+        color: #BC1E22;
+        margin: 0 42%;
+        border-radius: 10px;
+    }
+`;
+const StyledP = styled.p `
+    background-color: white;
+    margin: 0 47%;
+    border-radius: 10px;
+    ${props => props.type === 'height' ? 'color: #394A59' : 'color: #556567'}
+`;
 
 function Character(props) {
     console.log(props);
     return (
-        <div className = 'Characters'>
+        <StyledDiv className = 'container'>
             <h3>Name: {props.props.name}</h3> 
-        </div>
+            <br></br>
+            <StyledP type = 'height'>Height: {props.props.height}</StyledP>
+            <br></br>
+            <StyledP>Mass: {props.props.mass}</StyledP>
+            <br></br>
+        </StyledDiv>
     )
 };
 
