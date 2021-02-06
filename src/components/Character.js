15b27ee4a2c0c859de0b682bc1387eb3eeea2232
ACarrill42 <austincarrill@gmail.com>
2021-02-05 21:24:39 -0600

    working on props

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react';
+import App from '../App';
+
+function Character(props) {
+    const {} = props;
+    return (
+        <div className = 'Characters'>
+            <p>props.name</p>
+
+        </div>
+    )
+};
+
+export default Character;
