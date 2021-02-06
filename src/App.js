babc1a4601611a71955df37b2a797a39bdc7fbb7
ACarrill42 <austincarrill@gmail.com>
2021-02-05 22:15:51 -0600

    fixed props

--- src/App.js
+++ src/App.js
@@ -11,22 +11,22 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  useEffect(() => {
+  const fetchSWA = () => {
     axios.get('https://swapi.dev/api/people/')
     .then(res => {
       console.log(res);
-      setCharacters(res.data)
+      setCharacters(res.data);
     })
     .catch(err => {
-      console.log(err)
+      console.log(err);
     })
-  }, []);
-
+  }
+  useEffect(fetchSWA, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      { characters.map(info => <Character key ={info.name} info ={info}/>) }
+      { characters.map(info => <Character key ={info.name} props ={info}/>) }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,12 @@
 // Write your Character component here
 import React from 'react';
-import App from '../App';
+
 
 function Character(props) {
-    const {} = props;
+    console.log(props);
     return (
         <div className = 'Characters'>
-            <p>props.name</p>
-
+            <h3>Name: {props.props.name}</h3> 
         </div>
     )
 };
